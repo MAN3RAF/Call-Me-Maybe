7295c8650cf55bc79d6c585c8050f1a19051a06a
Lahcen Sebar <lsebar@c3r3p4.1337.ma>
2026-03-27 21:17:52 +0100

    up

--- src/validate.py
+++ src/validate.py
@@ -15,30 +15,44 @@ class FunctionValidator(BaseModel):
 	returns: Dict[str, str]
 
 
-class Validator:
 
-	def validate():
+def prompt_validate():
 
-		try:
-			with open("data/input/function_calling_tests.json", "r") as pr:
-				with open("data/input/functions_definition.json", "r") as fun:
-					# prompts = json.load(pr)
-					# for p in prompts:
-					# 	p_v = PromptValidator(prompt=p)
-					# 	print(p_v)
+	try:
+		with open("data/input/function_calling_tests.json", "r") as pr:
+			prompts = json.load(pr)
+			for p in prompts:
+				p_v = PromptValidator(prompt=p)
+				print(p_v)
 
-					funcs = json.load(fun)
-					for f in funcs:
-						f_v = FunctionValidator(name=f['name'], description=f['description'], parameters=f['parameters'], returns=f['returns'])
-						print(f_v)
 
-		except FileNotFoundError:
-			print("ERROR: No json file was found!")
-		except ValidationError:
-			print("Validation Error!")
-		except Exception:
-			print("ERROR: wrong json format!")
-		# print(prompts)
 
-v = Validator
-v.validate()
+	except FileNotFoundError:
+		print("ERROR: No json file was found!")
+	except ValidationError:
+		print("Validation Error!")
+	except Exception:
+		print("ERROR: wrong json format!")
+	# print(prompts)
+
+
+def func_validate():
+
+	try:
+		with open("data/input/functions_definition.json", "r") as fun:
+			
+			funcs = json.load(fun)
+			for f in funcs:
+				f_v = FunctionValidator(name=f['name'], description=f['description'], parameters=f['parameters'], returns=f['returns'])
+				print(f_v)
+
+
+	except FileNotFoundError:
+		print("ERROR: No json file was found!")
+	except ValidationError:
+		print("Validation Error!")
+	except Exception:
+		print("ERROR: wrong json format!")
+	# print(prompts)
+
+prompt_validate()
