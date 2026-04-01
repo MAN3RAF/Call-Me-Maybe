d5186fbebee06236c58fe46ea8967c2f8786a279
Lahcen Sebar <lsebar@c3r3p3.1337.ma>
2026-04-01 15:04:57 +0100

    up

--- src/test.py
+++ src/test.py
@@ -1,13 +1,36 @@
 from llm_sdk.llm_sdk import Small_LLM_Model
+from typing import List, Any
+import numpy as np
+
+def get_next_token(logits: List, allowed_ids: List) -> int:
+
+	tokens = np.array(logits) + -float("inf")
+
+	for id in allowed_ids:
+		tokens[id] = 0
+
+	constrained_logits = np.array(logits) + tokens
+	
+	print(constrained_logits)
 
 
-model = Small_LLM_Model()
 
-prompt = "Whats the sum of 2 and 4?"
 
-while True:
-	ids = model.encode(prompt)
 
-	logits = model.get_logits_from_input_ids(ids.tolist()[0])
+def main():
+	model = Small_LLM_Model()
 
-	print(f'{model.decode(logits.index(max(logits)))}')
+	prompt = "What is the sum of 2 and 4?"
+
+	while True:
+
+		ids = model.encode(prompt)
+
+		logits = model.get_logits_from_input_ids(ids.tolist()[0])
+
+		print(model.decode(logits.index(max(logits))))
+
+		prompt += model.decode(logits.index(max(logits)))
+
+
+get_next_token([1,2,3,4,5,6], [2,3])
